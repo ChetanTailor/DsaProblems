54d20783bc8535ee68e576358ffa2eb4d603ab57
Chetan Tailor <chetantailor764@gmail.com>
2022-08-07 00:20:20 +0530

    Dependencies

--- Programs/src/Graph/ShortestDistance.java
+++ Programs/src/Graph/ShortestDistance.java
@@ -1,2 +1,40 @@
-package Graph;public class ShortestDistance {
+package Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class ShortestDistance
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.minStep(n));
+        }
+    }
+}
+
+class Solution{
+    static int minStep(int n){
+        int e = 0;
+        while(n!=1)
+        {
+            if(n%3==0)
+            {
+                n=n/3;
+            }
+            else
+            {
+                n=n-1;
+            }
+            e++;
+        }
+        return e;
+    }
 }
